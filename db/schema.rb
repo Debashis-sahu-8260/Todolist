2ea76ba7abd3b4a101caa584d36d0adc278774b0
Debashis-sahu-8260 <debashissahu525@gmail.com>
2023-08-06 20:14:10 +0530

    some add code

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_05_104528) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_06_142722) do
   create_table "todolists", force: :cascade do |t|
     t.text "task"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
